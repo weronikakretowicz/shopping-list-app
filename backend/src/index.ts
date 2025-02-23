de8d72ab02f16e89394f72627effb5debf12af95
weronikakretowicz <235903@edu.p.lodz.pl>
2025-02-23 16:49:36 +0100

    Configure docker

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,32 +1,35 @@
-import { Hono } from 'hono';
-import { serve } from '@hono/node-server';
-import { connectDB } from './db';
-import { cors } from 'hono/cors';
-import { startWebSocketServer } from './websockets';
-import { createServer } from 'http';
-import { PORT } from './config';
-import {app} from "./app";
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
+import { connectDB } from "./db";
+import { cors } from "hono/cors";
+import { startWebSocketServer } from "./websockets";
+import { createServer } from "http";
+import { PORT } from "./config";
+import { app } from "./app";
 
 connectDB();
 
-app.use('*', cors());
-app.use('*', async (c, next) => {
-    c.res.headers.set('Content-Type', 'application/json');
-    await next();
+app.use("*", cors());
+app.use("*", async (c, next) => {
+  c.res.headers.set("Content-Type", "application/json");
+  await next();
 });
 
 // Tworzenie serwera HTTP dla WebSocketów
 const server = createServer();
 startWebSocketServer(server);
 
+// Routing
+app.get("/test", (c) => c.text("Hono!!"));
+
 // Uruchomienie serwera HTTP z API Hono
 serve({
-    fetch: app.fetch,
-    port: Number(PORT),
+  fetch: app.fetch,
+  port: Number(PORT),
 });
 
-server.listen(3002, () => {
-    console.log('📡 WebSocket server running on port 3002');
-});
+// server.listen(3002, () => {
+//     console.log('📡 WebSocket server running on port 3002');
+// });
 
 console.log(`🚀 Server is running on http://localhost:${PORT}`);
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,24 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-})
-
+  },
+  preview: {
+    port: 5173,
+    strictPort: true,
+  },
+  server: {
+    port: 5173,
+    // strictPort: true,
+    host: true,
+    // origin: "http://0.0.0.0:5173",
+  },
+});
